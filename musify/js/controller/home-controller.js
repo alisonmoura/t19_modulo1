cc5dadcd207f1bff1a0137403f2e9e0154727240
Alison Moura <alison.oghino@gmail.com>
2018-08-04 14:07:43 -0400

    Implementando for na listagem de música

--- musify/js/controller/home-controller.js
+++ musify/js/controller/home-controller.js
@@ -7,38 +7,30 @@ class HomeController {
         let musicas = model.buscar();
         let repeticoes = 0;
 
-        while (repeticoes < musicas.length) {
+        for (let i = 0; i < musicas.length; i++) {
             // Criar card da música
             var card = document.createElement("div"); // cria uma DIV
             card.classList.add("card-musica");
 
             var titulo = document.createElement("h2"); //cira um H2
             titulo.classList.add("titulo");
-            titulo.innerHTML = musicas[repeticoes].nome;
+            titulo.innerHTML = musicas[i].nome;
 
             var artista = document.createElement("h3"); //cira um H3
             artista.classList.add("artista");
-            artista.innerHTML = musicas[repeticoes].artista;
+            artista.innerHTML = musicas[i].artista;
 
             var duracao = document.createElement("h3"); //cira um H3
             duracao.classList.add("duracao");
-            duracao.innerHTML = musicas[repeticoes].duracao;
+            duracao.innerHTML = musicas[i].duracao;
 
             card.appendChild(titulo);
             card.appendChild(artista);
             card.appendChild(duracao);
 
             document.getElementById("lista-musicas").appendChild(card);
-            repeticoes++; // repeticoes = repeticoes + 1;
         }
 
-
-
-        // criar card da primeira música
-
-        // criar card da segunda música
-
-        // criar card da terceira música
     }
 
 }
